0d9b4810d4df28d6df91b681256a25b8e18df582
Akshay Iyer <ammasakshay@gmail.com>
2025-10-09 11:44:10 -0400

    VLN Plots

--- Scripts/16.CD8_Subclust.R
+++ Scripts/16.CD8_Subclust.R
@@ -85,11 +85,23 @@ for (res in resolutions) {
   )
 }
 
+for (col in grep("^wsnn_res\\.", colnames(tara_cdnk@meta.data), value = TRUE)) {
+  lvls <- as.character(sort(as.numeric(levels(tara_cdnk[[col]][, 1]))))
+  tara_cdnk[[col]] <- factor(tara_cdnk[[col]][, 1], levels = lvls)
+}
+
 setwd('/home/akshay-iyer/Documents/CD8_Longitudinal/T_Cell_Subsets')
+
+
+
 clustree(tara_cdnk, prefix = "wsnn_res.")
+
+
 ggsave('clustree.png',  width = 15,  # Adjust width as needed
        height = 9,  # Adjust height as needed
-       dpi = 300)
+       dpi = 300,
+       bg='white')
+
 
 DimPlot2(
   tara_cdnk,
@@ -101,4 +113,136 @@ DimPlot2(
 )
 
 ggsave('cd8nk_subcluster_res0.4.png',
-       dpi = 300)
+       dpi = 300,
+       bg='white')
+
+
+saveRDS(
+  tara_cdnk,
+  file = "/home/akshay-iyer/Documents/CD8_Longitudinal/saved_R_data/tara_cdnk.rds"
+)
+
+############################### ANNOTATION PLOTTING ##############################################
+
+# Base output directory
+out_dir <- "/home/akshay-iyer/Documents/CD8_Longitudinal/T_Cell_Subsets"
+
+# Create subfolders
+dir.create(out_dir, showWarnings = FALSE)
+dir.create(file.path(out_dir, "RNA_VLN"), showWarnings = FALSE)
+dir.create(file.path(out_dir, "ADT_VLN"), showWarnings = FALSE)
+dir.create(file.path(out_dir, "RNA_FeaturePlot"), showWarnings = FALSE)
+dir.create(file.path(out_dir, "ADT_FeaturePlot"), showWarnings = FALSE)
+
+rna.features <- c(
+  'ASCL2','BATF','BATF3','BCL6','C1QBP','CCL2','CCL3','CCL4L2','CCL5','CCND3','CD14','CD19','CD1C',
+  'CD200','CD27','CD3D','CD3E','CD36','CD4','CD40','CD40LG','CD70','CD7','CD79A','CD8A','CD8B',
+  'CLEC9A','CR2','CTLA4','CTSW','CXCL8','CXCR3','CXCR5','EBI3','ENTPD1','FABP5','FASLG','FAS','FCGR2B','FCGR3A',
+  'FCRL5','FOXP3','GNLY','GP1BA','GP9','GATA3','GZMK','HAVCR2','HIF1A','HIST1H4C','HLA-DPA1',
+  'HLA-DRA','HLA-DRB1','ICOS','IFI30','IFNG','IGFBP2','IGFBP4','IGHA1','IGHA2','IGHG1','IGHG2','IGHG3','IGHG4','IGHM','IKZF2','IL10',
+  'IL17A','IL18BP','IL18RAP','IL1B','IL21','IL2RA','IL2RB','IRF4','IRF8','ITGAX','JCHAIN','KLRB1',
+  'KLRD1','KLRC1','LAG3','LDHA','LGALS1','LTA','LTB','MAF','MAL','MALAT1','MIR155HG','MKI67',
+  'MT-ND1','MT-ND5','MS4A1','NELL2','NCAM1','NKG7','NR4A1','PDCD1','PF4','PPBP','PRDM1','PRF1',
+  'RORC','SELL','SERPINA1','SERPING1','SH2D1A','TCF4','TCF7','TIGIT','TNF','TNFAIP2','TNFRSF18',
+  'TNFRSF4','TNFRSF9','TOX','TBX21','TRBC1','TRDC','TRDV1','TRDV2','TRGC1','TRGC2','TRGV9','XBP1',
+  'XCL1','XCL2','ZBTB16','ZEB2'
+)
+
+prot.features <- rownames(tara_cdnk@assays$ADT)
+
+
+### RNA VLN PLOT
+DefaultAssay(tara_cdnk) <- 'RNA'
+for (gene in rna.features) {
+  if (gene %in% rownames(tara_cdnk[["RNA"]])) {
+    vln.pl <- VlnPlot2(
+      tara_cdnk,
+      features = gene,
+      group.by = 'wsnn_res.0.4',
+      cols = 'light',
+      show.mean = TRUE,
+      mean_colors = c("red", "blue")
+    ) + ggtitle(paste("RNA |", gene))
+    
+    ggsave(
+      filename = file.path(out_dir, "RNA_VLN", paste0(gene, "_VLN.png")),
+      plot = vln.pl,
+      dpi = 500, width = 14, height = 8, bg = "white"
+    )
+  }
+}
+
+### RNA Feature Plot
+for (gene in rna.features) {
+  if (gene %in% rownames(tara_cdnk[["RNA"]])) {
+    fp <- Dimplot2(
+      tara_cdnk,
+      features = gene,
+      group.by = 'wsnn_res.0.4',
+      reduction = "wnn.umap"
+    ) + ggtitle(paste("RNA |", gene))
+    
+    ggsave(
+      filename = file.path(out_dir, "RNA_FeaturePlot", paste0(gene, "_FeaturePlot.png")),
+      plot = fp,
+      dpi = 500, width = 8, height = 6, bg = "white"
+    )
+  }
+}
+
+### ADT VLN Plot
+DefaultAssay(tara_cdnk) <- 'ADT'
+
+for (prot in prot.features) {
+  if (prot %in% rownames(tara_cdnk[["ADT"]])) {
+    vln.pl <- VlnPlot2(
+      tara_cdnk,
+      features = prot,
+      group.by = 'wsnn_res.0.4',
+      assay = "ADT",
+      cols = 'light',
+      show.mean = TRUE,
+      mean_colors = c("red", "blue")
+    ) + ggtitle(paste("ADT |", prot))
+    
+    ggsave(
+      filename = file.path(out_dir, "ADT_VLN", paste0(prot, "_VLN.png")),
+      plot = vln.pl,
+      dpi = 500, width = 14, height = 8, bg = "white"
+    )
+  }
+}
+
+for (prot in prot.features) {
+  if (prot %in% rownames(tara_cdnk[["ADT"]])) {
+    fp <- Dimplot2(
+      tara_cdnk,
+      features = prot,
+      group.by = 'wsnn_res.0.4',
+      assay = "ADT",
+      reduction = "wnn.umap"
+    ) + ggtitle(paste("ADT |", prot))
+    
+    ggsave(
+      filename = file.path(out_dir, "ADT_FeaturePlot", paste0(prot, "_FeaturePlot.png")),
+      plot = fp,
+      dpi = 500, width = 8, height = 6, bg = "white"
+    )
+  }
+}
+
+TARA_ALL$Viral_Load
+TARA_entry <- subset(TARA_ALL, subset = Age %in% c(1, 2) & Condition == "HEI")  # VL is defined only for HEI
+TARA_entry$Viral_Load_Category <- ifelse(TARA_entry$Viral_Load >= 100000, "High", "Low")
+
+VlnPlot2(
+  TARA_entry,
+  features = 'HLA-C',
+  group.by = 'Manual_Annotation',
+  split.by = 'Viral_Load_Category',
+  assay = "RNA",
+  cols = 'light',
+  stat.method = 'wilcox.test',
+  show.mean = TRUE,
+  mean_colors = c("red", "blue")
+) 
